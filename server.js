6dc6ea97323d851e4c130d506d3fdebd0fbe2c7d
Esther Ola <estherolowo2018@gmail.com>
2025-04-11 15:04:18 +0100

    cors issues fixed

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // Routes
